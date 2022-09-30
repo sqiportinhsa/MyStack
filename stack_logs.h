c233d501faf0f8a3832e4d0839bfd0d75babab1f
sqiportinhsa <terekhova-olga-5d@yandex.ru>
2022-09-30 11:00:54 +0300

    UPD errors codes

--- stack_logs.h
+++ stack_logs.h
@@ -7,7 +7,8 @@
 #include "stack.h"
 #include "verification.h"
 
-#define DumpLogs(stk) RealDumpLogs(stk, __FILE__, __PRETTY_FUNCTION__, __LINE__, StackVerificator(stk))
+#define DumpLogs(stk) RealDumpLogs(stk, __FILE__, __PRETTY_FUNCTION__, __LINE__, \
+                                                          StackVerificator(stk))
 
 void RealDumpLogs(Stack *stk, const char *file, const char *func, int line, int errors);
 void Print(FILE *logs, const char *format, ...);
